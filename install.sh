ad482c830886408e2af4f7167458cf13cd44c2f4
Ziggy <zigs_17@hotmail.com>
2024-04-09 10:39:39 -0700

    Add btop, fastfetch, wofi. Other adjustments

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 echo "Updating system and installing packages..."
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm base-devel git zsh unzip
+sudo pacman -S --noconfirm base-devel git zsh unzip base
 
 # Install Yay
 echo "Installing Yay..."
@@ -25,12 +25,29 @@ fi
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Install Hyprland & Waybar
-sudo pacman -S hyprland waybar --noconfirm
+sudo pacman -S hyprland waybar hyprpaper kitty qt5-wayland qt6-wayland --noconfirm
 
 # Install more things
-sudo pacman -S openssh
+sudo pacman -S --noconfirm openssh lspci dunst which firefox man-db man-pages \
+    xclip bat btop pipewire pavucontrol ttf-font-awesome tmux ripgrep swaylock nodejs \
+    stow qt6-svg qt6-declarative fastfetch
 
-# TODO: detect GPU and install relevant drivers
+yay -S neovim-git sddm-git
+
+# Detect and install GPU drivers
+GPU=$(lspci | grep -E "VGA|3D")
+
+if echo "$GPU" | grep -qi "NVIDIA"; then
+    echo "NVIDIA GPU detected. Check the Wiki to proced: https://wiki.archlinux.org/title/NVIDIA"
+elif echo "$GPU" | grep -qi "AMD"; then
+    echo "AMD GPU detected. Installing AMD drivers..."
+    sudo pacman -S --noconfirm xf86-video-amdgpu vulkan-radeon
+elif echo "$GPU" | grep -qi "Intel"; then
+    echo "Intel GPU detected. Installing Intel drivers..."
+    sudo pacman -S --noconfirm mesa
+else
+    echo "No specific GPU detected, or GPU not recognized."
+fi
 
 # TODO: detect laptop and setup brightness
 
@@ -42,3 +59,8 @@ curl -fLo https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.0/JetBr
 unzip ~/Downloads/JetBrainsMono.zip -d ~/Downloads/jetbrains
 mv ~/Downloads/jetbrains/* ~/.local/share/fonts/
 fc-cache -fv
+
+# Set up Docker
+sudo pacman -S docker docker-compose --noconfirm
+sudo usermod -aG docker $USER
+sudo systemctl enable --now docker
