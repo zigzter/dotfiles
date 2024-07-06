c0e205de9257a34500fea37a895c56828423209c
Ziggy <zigs_17@hotmail.com>
2024-07-05 22:40:59 -0700

    Switch to Mocha, kwin window update

--- common/.config/nvim/lua/plugins/catppuccin.lua
+++ common/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,24 +1,24 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "macchiato",
-			integrations = {
-				dashboard = true,
-				harpoon = true,
-				leap = true,
-				mason = true,
-				mini = {
-					enabled = true,
-				},
-				noice = true,
-				telescope = {
-					enabled = true,
-				},
-				treesitter = true,
-			},
-		})
-	end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+            integrations = {
+                dashboard = true,
+                harpoon = true,
+                leap = true,
+                mason = true,
+                mini = {
+                    enabled = true,
+                },
+                noice = true,
+                telescope = {
+                    enabled = true,
+                },
+                treesitter = true,
+            },
+        })
+    end,
 }
