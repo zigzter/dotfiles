468dcdddcd8ea5cb8e0d17d06c524159af9df10e
Ziggy <zigs_17@hotmail.com>
2024-07-13 21:31:43 -0700

    Add git change jump keymaps for neovim

--- common/.config/nvim/lua/plugins/gitsigns.lua
+++ common/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,31 @@
 return {
     'lewis6991/gitsigns.nvim',
     config = function()
-        require('gitsigns').setup()
+        require('gitsigns').setup({
+            on_attach = function(bufnr)
+                local gitsigns = require('gitsigns')
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                map('n', ']c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ ']c', bang = true })
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
+
+                map('n', '[c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ '[c', bang = true })
+                    else
+                        gitsigns.nav_hunk('prev')
+                    end
+                end)
+            end
+        })
     end
 }
-
