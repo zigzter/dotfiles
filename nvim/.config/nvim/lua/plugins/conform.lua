a82a9396b7c0915f84cd90ad795409fa0c9ac77f
Ziggy <zigs_17@hotmail.com>
2025-05-21 16:09:12 -0700

    Update conform config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,41 +1,27 @@
 -- Formatting
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
-
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                json = { "prettier" },
-                vue = { "prettier" },
-                go = { "gofumpt" },
-                lua = { "stylua" },
-                css = { "prettier" },
-                md = { "mdformat" },
-                ruby = { "rubocop" },
-            },
-            format_after_save = {
-                lsp_fallback = true,
-                async = true,
-                timeout_ms = 2000,
-            },
-        })
-
-        vim.api.nvim_create_user_command("Format", function(args)
-            local range = nil
-            if args.count ~= -1 then
-                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-                range = {
-                    start = { args.line1, 0 },
-                    ["end"] = { args.line2, end_line:len() },
-                }
-            end
-            conform.format({ async = true, lsp_format = "fallback", range = range })
-        end, { range = true })
-    end,
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettierd", "prettier", "eslint_d", stop_after_first = true },
+        typescript = { "prettierd", "prettier", "eslint_d", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        json = { "prettier" },
+        vue = { "prettierd", "prettier" },
+        go = { "gofumpt" },
+        lua = { "stylua" },
+        css = { "prettier" },
+        md = { "mdformat" },
+        ruby = { "rubocop" },
+      },
+      format_after_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback"
+      }
+    })
+  end
 }
