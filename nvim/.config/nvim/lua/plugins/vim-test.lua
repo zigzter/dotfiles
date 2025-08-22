fde19c73c1479516f99dd0227f1b423ab75aaaf4
Ziggy <zigs_17@hotmail.com>
2025-08-22 10:18:51 -0700

    Fix vim-test

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,8 +1,13 @@
 -- Run tests from nvim
 return {
-    "vim-test/vim-test",
-    config = function()
-        vim.g["test#typescript#denotest#options"] = "--allow-read"
-        vim.g["test#javascript#denotest#options"] = "--allow-read"
-    end,
+  "vim-test/vim-test",
+  config = function()
+    vim.g["test#typescript#denotest#options"] = "--allow-read"
+    vim.g["test#javascript#denotest#options"] = "--allow-read"
+
+    -- Add --require test/setup.js for Node.js test runners
+    vim.g["test#javascript#mocha#options"] = "--require test/setup.js"
+    vim.g["test#javascript#jest#options"] = "--require test/setup.js"
+    vim.g["test#javascript#tap#options"] = "--require test/setup.js"
+  end,
 }
